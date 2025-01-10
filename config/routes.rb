0eeaa4a1c022ff07dad657d9cd9725244d83c641
vcosmin2701 <v.cosmin2717@gmail.com>
2025-01-11 00:01:35 +0200

    Adding subscribers to products

--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,7 @@ Rails.application.routes.draw do
 
   # delete "/products/:id", to: "products#destroy"
   root "products#index"
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
 end
