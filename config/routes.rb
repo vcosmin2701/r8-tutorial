fbede37de72d79655cfe0f4169bc407f68e00541
vcosmin2701 <v.cosmin2717@gmail.com>
2025-01-11 12:51:49 +0200

    Added subscribe and unsubscribe, need to fix unsubscribe

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,6 @@ Rails.application.routes.draw do
   root "products#index"
   resources :products do
     resources :subscribers, only: [ :create ]
+    resource :unsubscribe, only: [ :show ]
   end
 end
